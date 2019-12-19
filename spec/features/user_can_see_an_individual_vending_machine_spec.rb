06d0556f43158de8a03e81c0b650fe708cf0778d
Alison Vermeil <alison.minton@gmail.com>
2019-12-19 09:52:14 -0700

    Adjust test to show dollar values in view

--- spec/features/user_can_see_an_individual_vending_machine_spec.rb
+++ spec/features/user_can_see_an_individual_vending_machine_spec.rb
@@ -10,13 +10,13 @@ RSpec.describe 'When a user visits a vending machine show page', type: :feature 
     expect(page).to have_content("Don's Mixed Drinks Vending Machine")
   end
 
-  describe 'they see the name and price of all snacks for that machine' do
+  it 'they see the name and price of all snacks for that machine' do
     owner = Owner.create(name: "Sam's Snacks")
     dons  = owner.machines.create(location: "Don's Mixed Drinks")
 
-    burger = Snack.create!(name: "White Castle Burger", price: 3.50)
-    pop_rocks = Snack.create!(name: "Pop Rocks", price: 1.50)
-    cheetos = Snack.create!(name: "Flaming Hot Cheetos", price: 2.50)
+    burger = Snack.create!(name: "White Castle Burger", price_in_cents: 3.50)
+    pop_rocks = Snack.create!(name: "Pop Rocks", price_in_cents: 1.50)
+    cheetos = Snack.create!(name: "Flaming Hot Cheetos", price_in_cents: 2.50)
 
     dons.snacks << [burger, pop_rocks, cheetos]
 
@@ -24,17 +24,18 @@ RSpec.describe 'When a user visits a vending machine show page', type: :feature 
 
     within "#snack-#{burger.id}" do
       expect(page).to have_content(burger.name)
-      expect(page).to have_content(burger.price)
+      expect(page).to have_content("$3.50")
     end
 
     within "#snack-#{pop_rocks.id}" do
       expect(page).to have_content(pop_rocks.name)
-      expect(page).to have_content(pop_rocks.price)
+      expect(page).to have_content("$1.50")
     end
 
     within "#snack-#{cheetos.id}" do
       expect(page).to have_content(cheetos.name)
-      expect(page).to have_content(cheetos.price)
+      expect(page).to have_content("$2.50")
     end
+    save_and_open_page
   end
 end
