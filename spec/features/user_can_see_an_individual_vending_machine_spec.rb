b7de3cbbe3c2756c222325173383478cacd79176
Alison Vermeil <alison.minton@gmail.com>
2019-12-19 09:34:40 -0700

    Add test for displaying snacks name and price on vending machine show page

--- spec/features/user_can_see_an_individual_vending_machine_spec.rb
+++ spec/features/user_can_see_an_individual_vending_machine_spec.rb
@@ -9,4 +9,32 @@ RSpec.describe 'When a user visits a vending machine show page', type: :feature 
 
     expect(page).to have_content("Don's Mixed Drinks Vending Machine")
   end
+
+  describe 'they see the name and price of all snacks for that machine' do
+    owner = Owner.create(name: "Sam's Snacks")
+    dons  = owner.machines.create(location: "Don's Mixed Drinks")
+
+    burger = Snack.create!(name: "White Castle Burger", price: 3.50)
+    pop_rocks = Snack.create!(name: "Pop Rocks", price: 1.50)
+    cheetos = Snack.create!(name: "Flaming Hot Cheetos", price: 2.50)
+
+    dons.snacks << [burger, pop_rocks, cheetos]
+
+    visit machine_path(dons)
+
+    within "#snack-#{burger.id}" do
+      expect(page).to have_content(burger.name)
+      expect(page).to have_content(burger.price)
+    end
+
+    within "#snack-#{pop_rocks.id}" do
+      expect(page).to have_content(pop_rocks.name)
+      expect(page).to have_content(pop_rocks.price)
+    end
+
+    within "#snack-#{cheetos.id}" do
+      expect(page).to have_content(cheetos.name)
+      expect(page).to have_content(cheetos.price)
+    end
+  end
 end
